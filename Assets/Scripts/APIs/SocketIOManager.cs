d7d7893f0acd1c6626bf120efb76989fe16df6bc
EkanshBatra <>
2024-08-29 17:10:58 +0530

    Production Ready!!!

--- Assets/Scripts/APIs/SocketIOManager.cs
+++ Assets/Scripts/APIs/SocketIOManager.cs
@@ -33,7 +33,7 @@ public class SocketIOManager : MonoBehaviour
     internal JSHandler _jsManager;
 
     protected string SocketURI = null;
-    protected string TestSocketURI = "https://dev.casinoparadize.com";
+    protected string TestSocketURI = "https://game-crm-rtp-backend.onrender.com/";
     //protected string SocketURI = "http://localhost:5000";
 
     [SerializeField]
